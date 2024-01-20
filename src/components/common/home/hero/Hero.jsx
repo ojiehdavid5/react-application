9b194a5a3dafdf2fce55e124cd84002d1e4eca4b
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-20 22:16:32 +0100

    nothing new

--- src/components/common/home/hero/Hero.jsx
+++ src/components/common/home/hero/Hero.jsx
@@ -4,13 +4,25 @@ import './Hero.css'
 const Hero = () => {
   return (
     <>
-    <section className="class">
+    <section className="hero">
         <div className="container">
             <div className="row">
                 <p>hbhfhfmjdjwiiddjwdjdx k kwkqururb djsjsjlqlwjefnfnkkrk</p>
+                <div className="button">
+                  <button className="primary-btn">
+                    GET STARTED NOW       <i className='fa fa-long-arrow-alt-right '></i>
+
+                  </button>
+                  <button className="primary-btn">
+                    VIEW COURSE      <i className='fa fa-long-arrow-alt-right '></i>
+
+                  </button>
+            
+                </div>
             </div>
         </div>
     </section>
+    <div className="margin"></div>
     </>
   )
 }
