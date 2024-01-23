331a1e743fc0f0024ee3f25e93c525fbd268bc49
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-01-23 18:00:06 +0100

    about page

---
+++ src/components/about/AboutCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutCard = () => {
+  return (
+    <>
+    <h1>About card</h1>
+      
+    </>
+  )
+}
+
+export default AboutCard
--- src/components/common/back/Back.jsx
+++ src/components/common/back/Back.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
-const Back = ({title}) => {
-    const location=useLocation()
+const Back = ({ title }) => {
+  const location = useLocation()
   return (
     <div>
       <section className="back">
-        <h2>Home /  {location.pathname.spilt("/")[1]}</h2>
+        <h2>Home /{location.pathname.split("/")[1]}</h2>
         <h1>{title}</h1>
       </section>
     </div>
