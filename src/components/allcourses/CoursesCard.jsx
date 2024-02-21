955431c4cfacac9300c9425d957931190403ce06
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-02-21 11:32:37 +0100

    changes

--- src/components/allcourses/CoursesCard.jsx
+++ src/components/allcourses/CoursesCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+import './courses.css'
+
+import { coursesCard } from '../../dummydata'
+
 
 const CoursesCard = () => {
   return (
     <div>
         <section className="courseCard">
             <div className="container grid2">
-                {CoursesCard.map((val)=>{
+                {coursesCard.map((val)=>{
                     return(
                     <div className="items">
                         <div className="content flex">
@@ -25,6 +29,24 @@ const CoursesCard = () => {
                                 <i className='fa fa-star'></i>
                                 <i className='fa fa-star'></i>
                                 <label htmlFor="">(5.0)</label>
+                                </div>
+                                <div className="details">
+                                    
+                                    {val.courTeacher.map((details)=>(
+                                        <>
+                                        <div className="box">
+                                            <div className="dimg">
+                                                <img src={details.dcover} alt=""/>
+                                            </div>
+                                            <div className="para">
+                                                <h4>{details.name}</h4>
+                                            </div>
+                                        </div>
+                                        <span>{details.totalTime}</span>
+                                        </>
+                                    )
+
+                                    )}
                                 </div>
                             </div>
                             </div> 
