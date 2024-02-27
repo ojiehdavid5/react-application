fd97d0115620679f6b0ba9e94850b4b924066f0a
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-02-27 19:31:36 +0100

    changes

--- src/components/pricing/Faq.jsx
+++ src/components/pricing/Faq.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Title from "../common/title/Title"
 import { faq } from "../../dummydata"
 
 const Faq = () => {
+
+  const[click,setClick]=useState(false);
+    const toggle=(index)=>{
+      if(click=index){
+        return setClick(null)
+      }
+
+      setClick(index);
+    }
   return (
     <>
       <Title subtitle="FAQS" title="Frequently AskQuestion"/>
       <section className="faq">
         <div className="container">
-            {faq.map((val)=>(
+            {faq.map((val,index)=>(
                 <div className="box">
-                    <button>
+                    <button className='accordion'  onClick={()=>toggle(index)} key={index}>
                         <h2>{val.title}</h2>
+                        {/* <span>{click=index ? <iclassName"fa fa-chevron-down" }</span> */}
                     </button>
                     <div className="text">
                         <p>{val.desc}</p>
