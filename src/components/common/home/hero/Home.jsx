b554c05a5238851e5572bcafc0ea70f65ea88349
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-02 16:44:42 +0100

    changes

--- src/components/common/home/hero/Home.jsx
+++ src/components/common/home/hero/Home.jsx
@@ -4,7 +4,7 @@ import AboutCard from '../../../about/AboutCard'
 import HAbout from './HAbout'
 import Test from "../testimonials/Test"
 import Hblog from "../Hblog"
-import Hprice from "../Hprice"
+import Hprice from "./Hprice"
 
 const Home = () => {
   return (
